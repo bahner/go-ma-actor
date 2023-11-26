f83822f230df5cd4b73314a774eea79eaed74a4b
Lars Bahner <lars.bahner@gmail.com>
2023-11-26 21:30:21 +0100

    Add debug info to ew connection manager

--- p2p/connmgr/connmgr.go
+++ p2p/connmgr/connmgr.go
@@ -4,15 +4,23 @@ import (
 	"github.com/bahner/go-ma-actor/config"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
 	p2pConnmgr "github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	log "github.com/sirupsen/logrus"
 )
 
 func Init() (*p2pConnmgr.BasicConnMgr, error) {
 
-	withGracePeriod := connmgr.WithGracePeriod(config.GetConnMgrGracePeriod())
+	gracePeriod := config.GetConnMgrGracePeriod()
+	withGracePeriod := connmgr.WithGracePeriod(gracePeriod)
+	log.Infof("Connection manager grace period: %v", gracePeriod)
+
+	lowWaterMark := config.GetLowWaterMark()
+	log.Infof("Connection manager low water mark: %v", lowWaterMark)
+	highWaterMark := config.GetHighWaterMark()
+	log.Infof("Connection manager high water mark: %v", highWaterMark)
 
 	return p2pConnmgr.NewConnManager(
-		config.GetLowWaterMark(),
-		config.GetHighWaterMark(),
+		lowWaterMark,
+		highWaterMark,
 		withGracePeriod,
 	)
 
