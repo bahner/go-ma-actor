5d970cb360371114d20f5b41f5e38ef184b41307
Lars Bahner <lars.bahner@gmail.com>
2024-03-03 12:33:41 +0100

    Fix path

--- termux.sh
+++ termux.sh
@@ -2,7 +2,7 @@
 
 cd || exit
 
-export PATH="${PATH}:.local/.bin"
+export PATH="${PATH}:.local/bin"
 
 KUBO_VERSION="${KUBO_VERSION:-v0.26.0}"
 KUBO_TARBALL="kubo_${KUBO_VERSION}_linux-arm64.tar.gz"
@@ -10,7 +10,7 @@ KUBO_URL="https://dist.ipfs.tech/kubo/${KUBO_VERSION}/${KUBO_TARBALL}"
 
 GO_MA_ACTOR_VERSION="${GO_MA_ACTOR_VERSION:-v0.2.3}"
 GO_MA_ACTOR="go-ma-actor-android-arm64"
-GO_MA_ACTOR_URL=i"https://github.com/bahner/go-ma-actor/releases/download/${GO_MA_ACTOR_VERSION}/${GO_MA_ACTOR}.xz"
+GO_MA_ACTOR_URL="https://github.com/bahner/go-ma-actor/releases/download/${GO_MA_ACTOR_VERSION}/${GO_MA_ACTOR}.xz"
 pkg up
 
 ### IPFS
